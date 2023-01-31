618cc8a703c69fc3051903396a0653a53b926994
TimAbuev <tim-r10@yandex.ru>
2023-01-31 10:36:08 +0300

    feat: isShown-trash-icon-and-like-icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,9 +9,13 @@ function Card(props) {
   // Определяем, являемся ли мы владельцем текущей карточки
   const isOwn = cardContext.owner._id === currentUserContext._id;
 
-  // Далее в разметке используем переменную для условного рендеринга
-  
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = cardContext.likes.some(i => i._id === currentUserContext._id);
 
+  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = (
+    `elements__like ${isLiked && 'elements__like_active'}`
+  );
 
   function handleClick() {
     props.onCardClick(cardContext); //setPopupImageOpen(card)
@@ -23,12 +27,11 @@ function Card(props) {
       <button className="elements__button-wrapper" type="button" >
         <img alt="имя картинки" className="elements__image" src={`${cardContext.link}`} onClick={handleClick} />
       </button>
-      {/* <button className=""></button> */}
-      { isOwn && <button className='elements__trash' /> }
+      {isOwn && <button className='elements__trash' />}
       <div className="elements__wrapper">
         <h2 className="elements__title">{cardContext.name}</h2>
         <div className="elements__like-wrapper">
-          <button className="elements__like" type="button"></button>
+          <button className={cardLikeButtonClassName} type="button"></button>
           <p className="elements__counter">{cardContext.likes.length}</p>
         </div>
       </div>
