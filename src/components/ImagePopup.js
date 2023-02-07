393d9d0312fc668ec4f02c636ec18994d9e3992f
TimAbuev <tim-r10@yandex.ru>
2023-02-07 12:14:29 +0300

    fix: add-alt-to-imgPopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -3,7 +3,7 @@ function ImagePopup(props) {
     <div className={`popup popup-image ${props.isOpen ? 'popup_opened' : ''}`}>
       <div className="popup-image__wrapper">
         <button className="popup__close-icon" type="button" onClick={props.close}></button>
-        <img className="popup-image__image" alt="имя картинки"
+        <img className="popup-image__image" alt={props.selectedCard.name}
           src={`${props.selectedCard.link}`}
         />
         <p className="popup-image__caption">
